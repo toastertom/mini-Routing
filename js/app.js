f3d67a17b619ff990a07a07bb1ccac8384ede821
ToasterTom <tate.thomas5595@gmail.com>
2016-10-03 20:59:51 -0600

    General Update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
-angular.module('miniRouting', ['ui.router']);
+angular.module('miniRouting', ['ui.router'])
+
+.config(function ($stateProvider, $urlRouterProvider) {
+
+  $stateProvider                                  .state('home', {
+    templateUrl: 'homeTmpl.html',
+    controller: 'homeCtrl',
+    url: '/'
+  })
+  .state('settings', {
+    templateUrl: 'settingsTmpl.html',
+    controller: 'settingsCtrl.js',
+    url: '/settings'
+  })
+  .state('products', {
+    templateUrl: 'productTmpl.html',
+    controller: 'productsCtrl.js',
+    url: '/products/:id'
+  });
+
+// This directs all other routs to default.
+$urlRouterProvider.otherwise('/');
+
+});
