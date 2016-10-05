9bce4171c234278696035a9acc065b997f0e170e
ToasterTom <tate.thomas5595@gmail.com>
2016-10-04 19:25:41 -0600

    Added some Notes

--- js/products/productsCtrl.js
+++ js/products/productsCtrl.js
@@ -6,3 +6,7 @@ angular.module('miniRouting').controller('productsCtrl', function ($scope, $stat
         $scope.productData = productService.sockData;
     }
 });
+
+//$stateParams allows you to look at that specific URL.
+
+//productService is connecting this controller to that service. 
